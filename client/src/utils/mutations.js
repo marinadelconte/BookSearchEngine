e48f81a21c9cb748a0026119cac4ab1c0bbb36a8
Marina Del Conte <marinadelconte@yahoo.com>
2023-09-28 20:06:29 -0400

    added queries and mutations files to the front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($author: String!, $title: String!, $description: String, $image: String, $link: String) {
+    saveBook(author: $author, title: $title, description: $description, image: $image, link: $link) {
+      _id
+      authors
+      description
+      title
+      image
+      link 
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      authors
+      description
+      title
+      image
+      link 
+    }
+  }
+`;
